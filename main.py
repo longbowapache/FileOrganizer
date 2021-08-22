4353001459ce4b3afe858760fd0f7e3231dd7d66
Qize Xu <xxqqzztx@gmail.com>
2021-08-23 01:09:00 +0800

    add daemonize.

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
+#!/usr/bin/env python3
 import argparse
+
+import daemon
 
 from fileorganizer.action import MoveTo
 from fileorganizer.actionrunner import ActionRunner
 from fileorganizer.rule import ExtensionRule
 from fileorganizer.watcher import Watcher
 
-if __name__ == '__main__':
-    arg_parser = argparse.ArgumentParser(description='monitor directory and organize files')
-    arg_parser.add_argument('Path',
-                            metavar='path',
-                            type=str,
-                            help='the path to watch')
-    # Execute the parse_args() method
-    args = arg_parser.parse_args()
-    watch_path = args.Path
+
+def start_watcher(watch_path):
     Watcher(watch_path,
             ActionRunner([
                 (ExtensionRule('html'), MoveTo('HTML文件')),
@@ -25,3 +21,22 @@ if __name__ == '__main__':
                 (ExtensionRule('txt'), MoveTo('文本文件')),
             ])
             ).watch()
+
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(description='monitor directory and organize files')
+    arg_parser.add_argument('Path',
+                            metavar='path',
+                            type=str,
+                            help='the path to watch')
+    arg_parser.add_argument('-d',
+                            action="store_true",
+                            help='run as daemon')
+    # Execute the parse_args() method
+    args = arg_parser.parse_args()
+    watch_path = args.Path
+    if args.d:
+        with daemon.DaemonContext():
+            start_watcher(watch_path)
+    else:
+        start_watcher(watch_path)
